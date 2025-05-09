e0bcb808f33daff285fa51eb28e9cd706bf750eb
G5Atom <harryosullivan240@gmail.com>
2025-05-09 11:42:30 +0100

    Creating Scene Loader Mechanic
    First Script Attempt

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    private object other;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger Entered");
+
+        if(other.tag == "Player")
+        {
+            print("Switching Scene to" + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
